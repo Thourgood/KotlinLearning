4eebe413233008a29cd8919873a83e9291c502dd
Thourgood <jamesthourgood@gmail.com>
2023-08-10 20:31:44 +0100

    push 5

--- src/main/kotlin/KotlinChallenge3.kt
+++ src/main/kotlin/KotlinChallenge3.kt
@@ -1,6 +1,5 @@
 /* Given positive integer n implement a function which
 calculates sum of all numbers from 1 up to (and including) number n. */
-// Define a singleton object named "addUp"
 object AddUp {
     fun addUpTo(n: Int): Int {
         if (n == 1) {
